720175ede23e4d2dfedaf512bae453ed5e4bb689
Nofrisdan <nofrisdansitopu22@gmail.com>
2022-03-01 16:26:21 +0700

    Menghapus mv, formidable dan upload file

--- Routes/Route.js
+++ Routes/Route.js
@@ -1,9 +1,9 @@
 import express from "express";
-import { api, form, upload, upload_file } from "../Controllers/User.js";
+import { api, form, upload_file } from "../Controllers/User.js";
 const route = express.Router();
 
 route.get("/", form);
-route.post("/upload", upload);
+
 route.post("/upload_file", upload_file);
 
 route.get("/api", api);
